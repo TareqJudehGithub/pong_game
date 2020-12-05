80054bb22b7f2c1af5c6042df0493e20ec5a8374
Tareq Judeh <tareq.judeh@outlook.com>
2020-12-05 14:23:43 +0200

    Uploading project files to GitHub.

---
+++ main.py
@@ -0,0 +1,9 @@
+from turtle import Screen
+
+# TODO Setting up game screen:
+screen = Screen()
+screen.title("Pong")
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+
+screen.exitonclick()

75690caac5ec1705cfba6a80c50d50c6b273ec69
Tareq Judeh <tareq.judeh@outlook.com>
2020-12-05 19:30:19 +0200

    Created player1 and player2 paddle classes. In each class, we defined a paddle shape, color, dimensions, starting position, and movements.

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from turtle import Screen
+from time import sleep
+
+from paddle import P1Paddle, P2Paddle
+
+# Instances:
+screen = Screen()
+player_1 = P1Paddle()
+player_2 = P2Paddle()
 
 # TODO Setting up game screen:
-screen = Screen()
 screen.title("Pong")
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
+screen.tracer(0)
+
+# TODO Move paddles up and down:
+screen.listen()
+
+# Player 1:
+screen.onkey(player_1.p1_move_up, "Up")
+screen.onkey(player_1.p1_move_down, "Down")
+
+# Player 2:
+screen.onkey(player_2.p2_move_up, "w")
+screen.onkey(player_2.p2_move_down, "s")
+
+# Running the game:
+game_on = True
+while game_on:
+    screen.update()
+
 
 screen.exitonclick()

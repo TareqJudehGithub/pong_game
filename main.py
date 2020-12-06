fe8adce1b663eb99348c07f8fb1260b3b1f820ba
Tareq Judeh <tareq.judeh@outlook.com>
2020-12-06 02:28:58 +0200

    Updated Pong with Scoreboard class, which updates the game everytime ball gets out of bounds.

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from turtle import Screen
 from time import sleep
-from random import randint
 
 from paddle import P1Paddle, P2Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 # Instances:
 screen = Screen()
 player_1 = P1Paddle()
 player_2 = P2Paddle()
 ball = Ball()
+scoreboard = Scoreboard()
 
 # TODO Setting up game screen:
 screen.title("Pong")
@@ -25,38 +26,49 @@ screen.onkey(player_1.move_up, "Up")
 screen.onkey(player_1.move_down, "Down")
 
 # Player 2:
-screen.onkey(player_2.move_up, "w")
+screen.onkeypress(player_2.move_up, "w")
 screen.onkey(player_2.move_down, "s")
 
+
 # Running the game:
-game_on = True
-while game_on:
-    screen.update()
-    sleep(0.1)
-    ball.ball_move()
 
-    # Detect wall collision:
-    if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce_y()
+def pong_game():
+    game_on = True
+    while game_on:
+        screen.update()
+        sleep(ball.ball_speed)
+        ball.ball_move()
 
-    # Detect collision with Player 1:
-    if ball.distance(player_1) < 50 and ball.xcor() == 340:
-        ball.bounce_x()
-        print("P1 touches the ball!")
+        # Detect wall collision:
+        if ball.ycor() > 280 or ball.ycor() < -280:
+            ball.bounce_y()
 
-    if ball.distance(player_2) < 50 and ball.xcor() == -340:
-        print("P2 touches the ball!")
-        ball.bounce_x()
+        # Detect collision with Player 1:
+        if ball.distance(player_1) < 50 and ball.xcor() == 340:
+            ball.bounce_x()
+            ball.speed() + 1
+            print("P1 touches the ball!")
 
-    if ball.xcor() > 360:
-        print("P1 loses a ball!")
-        sleep(1)
-        ball.out_of_bounds()
+        if ball.distance(player_2) < 50 and ball.xcor() == -340:
+            print("P2 touches the ball!")
+            ball.bounce_x()
+            ball.speed() + 1
 
-    if ball.xcor() < -360:
-        print("P2 loses a ball!")
-        sleep(1)
-        ball.out_of_bounds()
+        if ball.xcor() > 380:
+            sleep(1)
+            ball.out_of_bounds()
+            scoreboard.p2_score_increase()
+            player_1.goto(x=350, y=0)
+            player_2.goto(x=-350, y=0)
 
+        if ball.xcor() < -380:
+            sleep(1)
+            ball.out_of_bounds()
+            scoreboard.p1_score_increase()
+            player_1.goto(x=350, y=0)
+            player_2.goto(x=-350, y=0)
+
+
+pong_game()
 
 screen.exitonclick()
